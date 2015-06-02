247828b850ab7b2e2dfdd880d4f3cd65a76e75e6
Ted Winslow <suicidolt@gmail.com>
2015-06-02 09:11:05 -0400

    fixing error
    scripts are created on first call
    should delete the scripts in a before block to make sure this works every time

--- server/api/script/script.spec.js
+++ server/api/script/script.spec.js
@@ -6,10 +6,10 @@ var request = require('supertest');
 
 describe('GET /api/v1/scripts', function() {
 
-  it('should respond with JSON array', function(done) {
+  it('should create initial scripts and respond with JSON array', function(done) {
     request(app)
       .get('/api/v1/scripts')
-      .expect(200)
+      .expect(201)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
