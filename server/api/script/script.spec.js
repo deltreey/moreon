6384a9e099d27f96583b6ca1e5971d672ae33077
deltreey <suicidolt@gmail.com>
2015-06-04 02:10:15 -0400

    fixing script test to pass every time

--- server/api/script/script.spec.js
+++ server/api/script/script.spec.js
@@ -3,18 +3,21 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var Script = require('./script.model');
 
 describe('GET /api/v1/scripts', function() {
 
-  it('should create initial scripts and respond with JSON array', function(done) {
-    request(app)
-      .get('/api/v1/scripts')
-      .expect(201)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
-        done();
-      });
+  it('should create initial scripts and respond with JSON array when there are no scripts in the db', function(done) {
+    Script.remove({}, function (err) {
+      request(app)
+        .get('/api/v1/scripts')
+        .expect(201)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) return done(err);
+          res.body.should.be.instanceof(Array);
+          done();
+        });
+    });
   });
 });
